515abca26830a38f46f00caac13ac24ce466eb6b
Lucas Brasil <lucasbrasil2804@gmail.com>
2026-06-17 10:29:40 -0300

    Aula_CRUD

--- Aula_CRUD/connection.php
+++ Aula_CRUD/connection.php
@@ -5,3 +5,11 @@ $user = "root";
 $password = "root";
 $database = "aula_sesi2";
 $port = 3307;
+
+try{
+    $conn = mysqli_connect($host, $user, $password, $database, $port);
+    echo "Conectando com o banco de dados do MySQL";
+}
+catch(Exception $e){
+    echo $e;
+}
